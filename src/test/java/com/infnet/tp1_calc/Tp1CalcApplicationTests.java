b8f5756a0e763889710ffa31da021436833e21b4
casdorio <casdorio@gmail.com>
2024-06-15 10:34:01 -0300

    first commit

---
+++ src/test/java/com/infnet/tp1_calc/Tp1CalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.tp1_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1CalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
